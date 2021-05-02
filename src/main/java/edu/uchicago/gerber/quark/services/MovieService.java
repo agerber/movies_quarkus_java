96780185ffb5eb9b9e5a405f63fb1dac7b3f4c06
Adam Gerber <adam.gerber@yahoo.com>
2021-05-01 19:45:21 -0500

    create service

---
+++ src/main/java/edu/uchicago/gerber/quark/services/MovieService.java
@@ -0,0 +1,23 @@
+package edu.uchicago.gerber.quark.services;
+
+import edu.uchicago.gerber.quark.models.Movie;
+
+import java.util.List;
+
+public class MovieService {
+
+    public List<Movie> findAll(){
+        //use repo to call findall
+        return null;
+    }
+    public List<Movie> add(Movie movie){
+        //use repo to call add
+        return null;
+    }
+    public Movie get(String id){
+        //use repo to get with id
+        return null;
+    }
+
+
+}
