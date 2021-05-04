32209fab44d3ca7d4a390157707cf78f3f7f75ec
Adam Gerber <adam.gerber@yahoo.com>
2021-05-04 12:23:50 -0500

    remove tests

--- src/test/java/edu/uchicago/gerber/quark/NativeExampleResourceIT.java
+++
@@ -1,9 +0,0 @@
-package edu.uchicago.gerber.quark;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeExampleResourceIT extends ExampleResourceTest {
-
-    // Execute the same tests but in native mode.
-}
